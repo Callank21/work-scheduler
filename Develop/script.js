a007b4053654e325cf40a0bdc0d6e97cd9c33409
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-03-24 17:33:15 -0700

    color coded the boxes

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,19 @@
 const date = dayjs().$d;
 const currentDay = document.querySelector("#currentDay");
 currentDay.textContent = dayjs(date).format('dddd, MMMM DD');
+const form = document.querySelectorAll(".time-block");
 
-$(".saveBtn").click(function() {
+console.log(dayjs(date).format('HH'));
 
-});
+for (var i = 0; i < form.length; i++) {
+    console.log(form[0].dataset.time);
+    if (dayjs(date).format('HH') > form[i].dataset.time) {
+        form[i].classList.add("past");
+    }
+    else if (dayjs(date).format('HH') == form[i].dataset.time) {
+        form[i].classList.add("present");
+    }
+    else if (dayjs(date).format('HH') < form[i].dataset.time) {
+        form[i].classList.add("future");
+    }
+}
